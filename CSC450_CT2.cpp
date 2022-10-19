8ae4ea59bc4c6b41ca2e5a88057a6c17aaf6d2ce
Joshua James <thecoloradothompsons@gmail.com>
2022-10-19 16:54:16 -0600

    Comments

--- CSC450_CT2.cpp
+++ CSC450_CT2.cpp
@@ -1,26 +1,31 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // Variables
     int NumOfStrings = 3;
     string buffer;
 
+    //Introduction & Directions
     cout << "Welcome to the String Reverser!" << endl;
     cout << "Let's reverse " << NumOfStrings << " strings" << endl;
     
+    //Loop NumOfStrings times, prompt for string
     for(int i = 1; i<=NumOfStrings; i++){
-        cout << "What is String #" << i << ": ";
+
+        cout << endl << "What is String #" << i << ": ";
         getline(cin, buffer);
 
         cout << "String " << i << " reversed is: ";
+        //Reverse string using for loop and index
         for(int j = buffer.length(); j >= 0; j--){
             cout << buffer[j];
         }  
         cout << endl;
     };
 
+    //Outtro
     cout << endl << "Goodbye!";    
     return 0;
 }
