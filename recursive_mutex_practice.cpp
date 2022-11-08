c9826834f95b68f122f7c8ffbbba3fea693e9d83
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-08 07:08:47 -0700

    recursive_mutex added

--- recursive_mutex_practice.cpp
+++ recursive_mutex_practice.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 
 struct Complex {
-    std::mutex mutex;
+    std::recursive_mutex mutex;
     int i;
 
     Complex() : i(5) {}
 
     void mul(int x){
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::recursive_mutex> lock(mutex);
         i *= x;
         std::cout << i << std::endl;
     }
 
     void div(int x){
-        std::lock_guard<std::mutex> lock(mutex);
+        std::lock_guard<std::recursive_mutex> lock(mutex);
         i /= x;
         std::cout << i << std::endl;
     }
 
     void both(int x, int y){
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::recursive_mutex> lock(mutex);
     mul(x);
     div(y);
     std::cout << i << std::endl;
