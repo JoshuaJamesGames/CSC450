9da082b339f3b2ac252e71b1c9f447a2689946e1
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-08 07:18:07 -0700

    CSC450_CT5 initial commit

---
+++ CSC450_CT5.cpp
@@ -0,0 +1,14 @@
+/*
+Program: Create a C++ program that will obtain input from a user and store it 
+into the provided CSC450_CT5_mod5.txtDownload CSC450_CT5_mod5.txt file. 
+
+Your program should append it to the provided text file, without deleting 
+the existing data:
+
+Store the provided data in the CSC450_CT5_mod5.txt file.
+
+Create a reversal method that will reverse all of the characters in the 
+CSC450_CT5_mod5.txt file and store the result in a 
+CSC450-mod5-reverse.txt file.
+*/
+
