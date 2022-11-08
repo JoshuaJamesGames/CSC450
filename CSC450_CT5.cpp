3f7686de818a370e36e22a2d60f54599281076c7
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-08 13:35:21 -0700

    reverseText added

--- CSC450_CT5.cpp
+++ CSC450_CT5.cpp
@@ -13,18 +13,73 @@ CSC450-mod5-reverse.txt file.
 */
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm>
 
 using namespace std;
 
+//Input validation function for file
+void fileInput(string fileName)
+{
+   //Input loop
+   while(true){
+      
+        string buffer;
+        fstream outputFile;
+
+        outputFile.open(fileName, ios::out | ios::app);
+        cout << "What would you like to add to " << fileName << "?" << endl;
+
+        getline(cin, buffer);
+
+        if(cin.good()){
+            outputFile << endl << buffer;
+            cout << "\nYour comments have been added" << endl;
+            break;
+        //Otherwise, output feedback and clear input buffer
+        }else{
+            cout << "\nIncorrect Input!" << endl;
+            cout << "Please enter a valid String: ";            
+        }
+        outputFile.close();
+   }
+}
+
+void reverseText(string inputFileName, string outputFileName)
+{
+    string buffer;
+    ifstream inputFile;
+    ofstream outputFile;
+
+    inputFile.open(inputFileName, ios::in);
+    outputFile.open(outputFileName, ios::out);
+
+    cout << "\nCreated " << outputFileName << " concatenating reversed text" << endl;
+
+    while(getline(inputFile, buffer)){
+        reverse(buffer.begin(), buffer.end());
+        outputFile << buffer << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+}
 
 int main(int argc, char const *argv[])
 {
-    string requiredFile = "CSC450_CT5.mod5.txt";
-
+    const string requiredFile = "CSC450_CT5.mod5.txt";
+    const string reverseOutputFile = "CSC450-mod5-reverse.txt";
+    
     //Intro
     cout << "Welcome to the User Input Program" << endl;
     cout << "Opening " << requiredFile << endl;
 
+    //User input
+    fileInput(requiredFile);
+
+    //Reverse requiredFile into reverseOutputFile
+    reverseText(requiredFile, reverseOutputFile);
 
     //Outtro
     cout << "\nGoodbye!";
