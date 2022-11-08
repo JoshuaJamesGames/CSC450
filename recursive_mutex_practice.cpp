efca008ebfdb48d4de0b48646ba86341d2bb228f
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-08 06:31:21 -0700

    Recursive Mutex Lock Practice

---
+++ recursive_mutex_practice.cpp
@@ -0,0 +1,37 @@
+#include <mutex>
+#include <iostream>
+
+struct Complex {
+    std::mutex mutex;
+    int i;
+
+    Complex() : i(5) {}
+
+    void mul(int x){
+        std::lock_guard<std::mutex> lock(mutex);
+        i *= x;
+        std::cout << i << std::endl;
+    }
+
+    void div(int x){
+        std::lock_guard<std::mutex> lock(mutex);
+        i /= x;
+        std::cout << i << std::endl;
+    }
+
+    void both(int x, int y){
+    std::lock_guard<std::mutex> lock(mutex);
+    mul(x);
+    div(y);
+    std::cout << i << std::endl;
+}
+};
+
+int main(){
+    Complex complex;
+    complex.mul(4);
+    complex.div(2);
+    complex.both(32, 23);
+
+    return 0;
+}
