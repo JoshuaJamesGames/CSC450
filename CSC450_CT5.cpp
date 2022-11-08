c1e06523b99b41d47252cbe4fa91dcbe6908a506
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-08 12:06:14 -0700

    Outline sections intro & outtro

--- CSC450_CT5.cpp
+++ CSC450_CT5.cpp
@@ -1,6 +1,6 @@
 /*
 Program: Create a C++ program that will obtain input from a user and store it 
-into the provided CSC450_CT5_mod5.txtDownload CSC450_CT5_mod5.txt file. 
+into the provided CSC450_CT5_mod5.txt 
 
 Your program should append it to the provided text file, without deleting 
 the existing data:
@@ -11,4 +11,23 @@ Create a reversal method that will reverse all of the characters in the
 CSC450_CT5_mod5.txt file and store the result in a 
 CSC450-mod5-reverse.txt file.
 */
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    string requiredFile = "CSC450_CT5.mod5.txt";
+
+    //Intro
+    cout << "Welcome to the User Input Program" << endl;
+    cout << "Opening " << requiredFile << endl;
+
+
+    //Outtro
+    cout << "\nGoodbye!";
+
+    return 0;
+}
