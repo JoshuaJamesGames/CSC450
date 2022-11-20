84cf10868a7726567e56794e9483967558e85e5a
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-20 07:54:50 -0700

    CSC450_CT6_NC1 implemented

---
+++ CSC450_CT6_NC1.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+import java.io.IOException;
+
+class Password {
+    public static void main (String args[]) throws IOException {
+      Console c = System.console();
+      if (c == null) {
+        System.err.println("No console.");
+        System.exit(1);
+      }
+   
+      String username = c.readLine("Enter your user name: ");
+      String password = c.readLine("Enter your password: ");
+   
+      if (!verify(username, password)) {
+        throw new SecurityException("Invalid Credentials");
+      }
+   
+      // User is authorized, continue...
+    }
+   
+    // Dummy verify method, always returns true  
+    private static final boolean verify(String username, String password) {
+      return true;
+    }
+  }
+  
