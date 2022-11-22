81233abae8e316febf2092998bd9449a092ea639
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-22 07:30:21 -0700

    Concurrency implemented

--- CSC450_Portfolio_Project_Pt1.cpp
+++ CSC450_Portfolio_Project_Pt1.cpp
@@ -2,33 +2,33 @@
 #include <thread>
 using namespace std;
 
-void countUp(int *i){
+void countUp(int &i){
 
-    while(*i<=100){
-        cout<< *i++ << " ";
+    while(i<=100){
+        cout<< i << " ";
+        i++;
     }
     cout<< "\nDone counting up!" << endl;
 
 }
 
-void countDown(int *i){
-    while(*i>=0){
-        cout<< *i-- << " ";
+void countDown(int &i){
+    while(i>=0){
+        cout<< i << " ";
+        i--;
     }
     cout<< "\nDone counting down!" << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    int* i = new int(0);
+    int i = 0;
 
     thread t1(countUp, ref(i));
     t1.join();
 
     thread t2(countDown, ref(i));
     t2.join();
-
-    delete i;
 
     return 0;
 }
