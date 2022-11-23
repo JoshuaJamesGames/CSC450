fba45776ff1d09802e686018b84d199c1d58823f
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-23 07:17:09 -0700

    Reversed order of threads per instructions

--- CSC450_Portfolio_Project_Pt1.cpp
+++ CSC450_Portfolio_Project_Pt1.cpp
@@ -4,30 +4,32 @@ using namespace std;
 
 void countUp(int &i){
 
-    while(i<=100){
+    while(i<100){
         cout<< i << " ";
         i++;
     }
+    cout<< i;
     cout<< "\nDone counting up!" << endl;
 
 }
 
 void countDown(int &i){
-    while(i>=0){
+    while(i>0){
         cout<< i << " ";
         i--;
     }
+    cout<< i;
     cout<< "\nDone counting down!" << endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    int i = 0;
+    int i = 100;
 
-    thread t1(countUp, ref(i));
+    thread t1(countDown, ref(i));
     t1.join();
 
-    thread t2(countDown, ref(i));
+    thread t2(countUp, ref(i));
     t2.join();
 
     return 0;
