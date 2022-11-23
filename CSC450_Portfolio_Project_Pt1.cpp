cccd9a4b7c9a38d5346ed69967ca5d9249afca14
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-23 07:19:38 -0700

    Comments

--- CSC450_Portfolio_Project_Pt1.cpp
+++ CSC450_Portfolio_Project_Pt1.cpp
@@ -8,6 +8,7 @@ void countUp(int &i){
         cout<< i << " ";
         i++;
     }
+    //Output final digit outside of loop
     cout<< i;
     cout<< "\nDone counting up!" << endl;
 
@@ -18,18 +19,24 @@ void countDown(int &i){
         cout<< i << " ";
         i--;
     }
+    //Output final digit outside of loop
     cout<< i;
     cout<< "\nDone counting down!" << endl;
 }
 
 int main(int argc, char const *argv[])
 {
+    //Initialize variable
     int i = 100;
 
+    //Call first thread and pass i by reference
     thread t1(countDown, ref(i));
+    //Wait until t1 finishes
     t1.join();
 
+    //Call second thread and pass i by reference
     thread t2(countUp, ref(i));
+    //Wait until t2 finishes
     t2.join();
 
     return 0;
