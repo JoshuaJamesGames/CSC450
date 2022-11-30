42352fd776b6a4268b113c3743316e3d16142152
JoshuaJamesGames <thecoloradothompsons@gmail.com>
2022-11-30 07:44:44 -0700

    Added nanoTime for performance and comments

--- CSC450_Portfolio_Project.java
+++ CSC450_Portfolio_Project.java
@@ -2,12 +2,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class CSC450_Portfolio_Project{
     public static void main(String[] args) {
+        //Create an atomic integer to share between runnable objects
         AtomicInteger sharedNum = new AtomicInteger(100);
 
+        //Tracking execution time in nano seconds
+        long startTime = System.nanoTime();
+
+        //Instantiate and run each object using shared integer
+        //First count down
         countDown countDown = new countDown(sharedNum);
         countDown.run();
+
+        //Then count up
         countUp countUp = new countUp(sharedNum);
         countUp.run();
+
+        //Acquire end time & output
+        long endTime = System.nanoTime();
+        System.out.println("Total time: " + (endTime - startTime) + " nano Seconds");
     }
 
 }
